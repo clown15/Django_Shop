954e7e1b19e54571512df203ff734b43be43a2ad
GiBum Park <thsdhrhd15@naver.com>
2020-11-04 21:24:57 +0900

    Model구성

--- product/models.py
+++ product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, verbose_name="상품명")
+    price = models.IntegerField(verbose_name="상품가격")
+    description = models.TextField(verbose_name="상품설명")
+    stuck = models.IntegerField(verbose_name="재고")
+    reguster_date = models.DateTimeField(
+        auto_now_add=True, verbose_name="등록일자")
+
+    class Meta:
+        db_table = 'fc_product'
+        verbose_name = '상품'
+        verbose_name_plural = '상품'
