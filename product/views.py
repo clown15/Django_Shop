1903ddd39ee0706810e792533d2b2f2954f1d640
clown15 <thsdhrhd15@naver.com>
2021-02-09 08:35:19 +0000

    상품 상세보기 페이지 추가

--- product/views.py
+++ product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
@@ -8,9 +8,15 @@ from .forms import RegisterForm
 class ProductList(ListView):
     model = Product
     template_name="product.html"
+    # html에서 사용할 변수 명
     context_object_name="product_list"
 
 class ProductCreate(FormView):
     template_name = "register_product.html"
     form_class=RegisterForm
     success_url = "/product/"
+
+class ProduectDetail(DetailView):
+    template_name = "product_detail.html"
+    queryset = Product.objects.all()
+    context_object_name = "product"
