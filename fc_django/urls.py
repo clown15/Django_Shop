62c5fb6013375c8ec6655e451769d7654dacefb0
clown15 <thsdhrhd15@naver.com>
2021-02-18 07:48:07 +0000

    add RestAPI Framework

--- fc_django/urls.py
+++ fc_django/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from fcuser.views import index,RegisterView,LoginView,logout
-from product.views import ProductList,ProductCreate,ProduectDetail
+from product.views import (
+    ProductList,ProductCreate,ProduectDetail,
+    ProductListAPI,ProductDetailAPI
+)
 from order.views import OrderCreate,OrderList
 
 urlpatterns = [
@@ -30,4 +33,6 @@ urlpatterns = [
     path('product/<int:pk>/',ProduectDetail.as_view()),
     path('order/',OrderList.as_view()),
     path('order/create/',OrderCreate.as_view()),
+    path('api/product',ProductListAPI.as_view()),
+    path('api/product/<int:pk>',ProductDetailAPI.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -6,7 +6,30 @@ from .forms import RegisterForm
 from order.forms import RegisterForm as OrderForm
 from django.utils.decorators import method_decorator
 from fcuser.decorators import admin_required
+from rest_framework import generics
+from rest_framework import mixins
+from .serializers import ProductSerializer
 # Create your views here.
+
+class ProductListAPI(generics.GenericAPIView,mixins.ListModelMixin):
+    serializer_class = ProductSerializer
+
+    # queryset = Product.objects.all()
+    def get_queryset(self):
+        return Product.objects.all().order_by("id")
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+
+class ProductDetailAPI(generics.GenericAPIView,mixins.RetrieveModelMixin):
+    serializer_class = ProductSerializer
+
+    # queryset = Product.objects.all()
+    def get_queryset(self):
+        return Product.objects.all().order_by("id")
+
+    def get(self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
 
 class ProductList(ListView):
     model = Product
