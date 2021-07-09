3b209f8a31084d0cb904dbb1f314885e822394c5
clown15 <thsdhrhd15@naver.com>
2021-07-09 01:48:05 +0000

    add user, product page

---
+++ product/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from .models import Product
+from user.models import User
+
+class Product_Register(forms.Form):
+    def __init__(self,request,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.request = request
+
+    name = forms.CharField(
+        error_messages = {
+            'required':'상품명을 입력하세요.'
+        },max_length=256,
+        label = "상품명"
+    )
+    price = forms.IntegerField(
+        error_messages = {
+            'required':'가격을 입력하세요.'
+        },
+        label = "가격"
+    )
+    stock = forms.IntegerField(
+        error_messages = {
+            'required':'수량을 입력하세요.'
+        },
+        label = '수량'
+    )
+    info = forms.CharField(
+        widget=forms.Textarea,
+        label = "상품설명"
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        price = cleaned_data.get('price')
+        stock = cleaned_data.get('stock')
+        info = cleaned_data.get('info')
+
+        product = Product(
+            name = name,
+            price = price,
+            stock = stock,
+            info = info,
+            creator = User.objects.get(email=self.request.session.get('user')),
+        )
+        product.save()
---
+++ user/forms.py
@@ -0,0 +1,97 @@
+from django import forms
+from .models import User
+from django.contrib.auth.hashers import make_password,check_password
+
+class SignUpForm(forms.Form):
+    email = forms.EmailField(
+        error_messages = {
+            'required':'이메일을 입력하세요.'
+        },
+        label = "이메일",
+    )
+    password = forms.CharField(
+        error_messages = {
+            'required':'비밀번호를 입력하세요.'
+        },
+        widget = forms.PasswordInput,
+        label = "비밀번호",
+    )
+    re_password = forms.CharField(
+        error_messages = {
+            'required':'비밀번호를 입력하세요.'
+        },
+        widget = forms.PasswordInput,
+        label = "비밀번호 확인",
+    )
+    level = forms.ChoiceField(
+        error_messages = {
+            'required':'등급을 선택하세요.'
+        },
+        choices = (
+            # (실제 값, 보여지는 값)
+            ("admin","관리자"),
+            ("user","고객"),
+        ),
+        # 초기값 설정
+        initial = "user",
+        label = "등급",
+    )
+    # level = forms.CharField(
+    #     widget=forms.Select(choices=(
+            # ("admin","관리자"),
+            # ("user","고객"),
+        # )),
+    # )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+        re_password = cleaned_data.get('re_password')
+        level = cleaned_data.get('level')
+
+        # 두 값이 있는지 확인 후 값이 같은지 확인
+        if password and re_password:
+            if password != re_password:
+                self.add_error('password','비밀번호가 다릅니다.')
+                self.add_error('re_password','비밀번호가 다릅니다.')
+            else:
+                user = User(
+                    email = email,
+                    password = make_password(password),
+                    level = level
+                )
+
+                user.save()
+
+class SignInForm(forms.Form):
+    email = forms.EmailField(
+        error_messages = {
+            'required':'이메일을 입력하세요.'
+        },
+        label = "이메일",
+    )
+    password = forms.CharField(
+        error_messages = {
+            'required':'비밀번호를 입력하세요.'
+        },
+        widget = forms.PasswordInput,
+        label = "비밀번호",
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+
+            if not check_password(password,user.password):
+                self.add_error('password','비밀번호가 다릅니다.')
+
+            else:
+                self.email = user.email
+        
+        except Fcuser.DoesNotExist:
+            self.add_error("email","아이디가 존재하지 않습니다.")
